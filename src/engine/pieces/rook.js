76b5b2cf5625e3747e24e339b14386eb77b634c9
Simon Duck <its.duck@gmail.com>
2025-04-29 14:52:52 +0100

    red 3 test complete

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -1,11 +1,29 @@
-import Piece from './piece';
+import Piece from "./piece";
+import Square from "../square";
 
 export default class Rook extends Piece {
-    constructor(player) {
-        super(player);
-    }
+  constructor(player) {
+    super(player);
+  }
 
-    getAvailableMoves(board) {
-        return new Array(0);
+  getAvailableMoves(board) {
+    let location = board.findPiece(this);
+    // location.row, location.col
+    let rookAvailableMoves = [];
+    for (let i = 0; i < location.col; i++) {
+      rookAvailableMoves.push(Square.at(location.row, i));
     }
+    for (let i = location.col + 1; i <= 7; i++) {
+      rookAvailableMoves.push(Square.at(location.row, i));
+    }
+    for (let i = 0; i < location.row; i++) {
+      rookAvailableMoves.push(Square.at(i, location.col));
+    }
+    for (let i = location.row + 1; i <= 7; i++) {
+      rookAvailableMoves.push(Square.at(i, location.col));
+    }
+    console.log(rookAvailableMoves);
+
+    return rookAvailableMoves;
+  }
 }
