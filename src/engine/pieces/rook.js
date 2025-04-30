4399f3fda388a2fda978abf15c03bbf6fc8f1709
Simon Duck <its.duck@gmail.com>
2025-04-30 16:02:43 +0100

    red 13

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -1,5 +1,6 @@
 import Piece from "./piece";
 import Square from "../square";
+import Player from "../player";
 
 export default class Rook extends Piece {
   constructor(player) {
@@ -8,7 +9,6 @@ export default class Rook extends Piece {
 
   getAvailableMoves(board) {
     let location = board.findPiece(this);
-    // location.row, location.col
     let rookAvailableMoves = [];
 
     for (let i = location.col - 1; i >= 0; i--) {
@@ -16,43 +16,59 @@ export default class Rook extends Piece {
 
       if (curPiece == undefined) {
         rookAvailableMoves.push(Square.at(location.row, i));
+      } else if (
+        curPiece.player != this.player &&
+        curPiece.constructor.name != "King"
+      ) {
+        rookAvailableMoves.push(Square.at(location.row, i));
+        break;
+      } else {
+        break;
       }
-      
-      else  {break}
-      }
-      
-     
-    for (let i = location.col +1; i <= 7; i++) {
+    }
+
+    for (let i = location.col + 1; i <= 7; i++) {
       let curPiece = board.getPiece(Square.at(location.row, i));
       if (curPiece == undefined) {
         rookAvailableMoves.push(Square.at(location.row, i));
+      } else if (
+        curPiece.player != this.player &&
+        curPiece.constructor.name != "King"
+      ) {
+        rookAvailableMoves.push(Square.at(location.row, i));
+        break;
+      } else {
+        break;
       }
-
-      else  {break}
-      
-     
     }
-    for (let i = location.row +1; i <= 7 ; i++) {
+    for (let i = location.row + 1; i <= 7; i++) {
       let curPiece = board.getPiece(Square.at(i, location.col));
       if (curPiece == undefined) {
-
         rookAvailableMoves.push(Square.at(i, location.col));
+      } else if (
+        curPiece.player != this.player &&
+        curPiece.constructor.name != "King"
+      ) {
+        rookAvailableMoves.push(Square.at(location.row, i));
+        break;
+      } else {
+        break;
       }
-
-      else  {break}
-      
     }
     for (let i = location.row - 1; i >= 0; i--) {
       let curPiece = board.getPiece(Square.at(i, location.col));
       if (curPiece == undefined) {
         rookAvailableMoves.push(Square.at(i, location.col));
-        
-      }
-      else {
+      } else if (
+        curPiece.player != this.player &&
+        curPiece.constructor.name != "King"
+      ) {
+        rookAvailableMoves.push(Square.at(location.row, i));
         break;
-    }
+      } else {
+        break;
       }
-      
+    }
 
     return rookAvailableMoves;
   }
